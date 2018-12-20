fab098d458a6673895eccd225097d0311b4b2ea4
ruleless <ruleless@126.com>
2018-12-20 21:59:21 +0800

    nginx sample module

--- ngx_http_dump/ngx_http_dump_module.c
+++ ngx_http_dump/ngx_http_dump_module.c
@@ -7,6 +7,7 @@ typedef struct {
 } ngx_http_dump_loc_conf_t;
 
 
+static char *ngx_http_dump(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_dump_init(ngx_conf_t *cf);
 static void *ngx_http_dump_create_loc_conf(ngx_conf_t *cf);
 static char *ngx_http_dump_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
@@ -15,7 +16,7 @@ static char *ngx_http_dump_merge_loc_conf(ngx_conf_t *cf, void *parent, void *ch
 static ngx_command_t ngx_http_dump_commands[] = {
     { ngx_string("dump_all"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_flag_slot,
+      ngx_http_dump,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_dump_loc_conf_t, dump_all),
       NULL },
@@ -58,6 +59,12 @@ static ngx_http_output_body_filter_pt    ngx_http_next_body_filter;
 
 
 static ngx_int_t
+ngx_http_dump_hanlder(ngx_http_request_t *r)
+{
+    return NGX_DECLINED;
+}
+
+static ngx_int_t
 ngx_http_dump_header_filter(ngx_http_request_t *r)
 {
     return ngx_http_next_header_filter(r);
@@ -67,6 +74,26 @@ static ngx_int_t
 ngx_http_dump_body_filter(ngx_http_request_t *r, ngx_chain_t *in)
 {
     return ngx_http_next_body_filter(r, in);
+}
+
+static char *
+ngx_http_dump(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_dump_loc_conf_t *dlcf = conf;
+    ngx_http_core_loc_conf_t *clcf;
+    char *rv;
+
+    rv = ngx_conf_set_flag_slot(cf, cmd, conf);
+    if (rv != NGX_CONF_OK) {
+        return rv;
+    }
+
+    if (dlcf->dump_all) {
+        clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+        clcf->handler = ngx_http_dump_hanlder;
+    }
+
+    return NGX_CONF_OK;
 }
 
 static ngx_int_t
